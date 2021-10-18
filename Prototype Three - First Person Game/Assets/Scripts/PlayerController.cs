260e3d9ca4fd4f2c271e6d78e79b25c29f64fb7c
Isaac <10804540@uvu.edu>
2021-10-17 21:58:41 -0600

    Shooting And Movement
    - Added Proper Movement
    - Added Jumping
    - Added Basic Shooting Mechanic
    - Bullets Destroy Themselves When Far Away
    - Cursor Locks and Camera Behaves Properly

--- Prototype Three - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype Three - First Person Game/Assets/Scripts/PlayerController.cs
@@ -10,33 +10,60 @@ public class PlayerController : MonoBehaviour
         lookSens,           //Mouse Camera Control Sensitivity
         minLook,            //Highest Vertical Camera Angle
         maxLook;            //Lowest Vertical Camera Angle
-    private float rotX;     //Current X Camera Rotation
+    private float rotX;
     private Camera cam;
     private Rigidbody rb;
+    private Transform launcher;
 
     void Start()
     {
         //Get Components
-        //camera = camera.main;
+        cam = Camera.main;
         rb = GetComponent<Rigidbody>();
+        launcher = GameObject.Find("Blast").transform;
     }
 
     void Update()
     {
         Move();
+        CamLook();
+        Jump();
+        Shoot();
     }
 
     void Move()             //Player Movement
     {
-        float x = Input.GetAxis("Horizontal") * moveSpeed;
-        float z = Input.GetAxis("Vertical") * moveSpeed;
-
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(x, 0, z);
     }
 
     void CamLook()          //Camera Rotation
     {
         float x = Input.GetAxis("Mouse X") * lookSens;
-        rotX += Input.GetAxis("Mouse X") * lookSens;
+        rotX += Input.GetAxis("Mouse Y") * lookSens;
+        float clamp = Mathf.Clamp(rotX, minLook, maxLook); 
+        transform.Rotate(0, x, 0);
+        cam.transform.localRotation = Quaternion.Euler(-clamp, 0, 0);
+    }
+
+    void Awake()
+    {
+        //Disable Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Jump()             //Jumping
+    {
+        if (Input.GetButtonDown("Jump")) rb.velocity = new Vector3(0, jumpForce, 0);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown("mouse 0"))
+        {
+            Instantiate((Resources.Load("Bullet")), launcher.transform.position, launcher.transform.rotation);
+            print("Bang!");
+        }
     }
 }
